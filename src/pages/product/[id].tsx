836b5690810d8eda0f43376d636013964759edf7
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-09-25 18:23:48 -0300

    get product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,19 +1,87 @@
-import type { NextPage } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
+import Image from "next/future/image";
+import { PriceProps, stripe } from "../../lib/stripe";
+
+import {
+  ImageContainer,
+  Loading,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
+import { formattedCurrency } from "../../utils/formattedCurrency";
 import { useRouter } from "next/router";
 
-const Product: NextPage = () => {
-  const { query } = useRouter();
+interface Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  description: string;
+  price: string;
+}
+
+interface ProductProps {
+  product: Product;
+}
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <Loading>Carregando Produto...</Loading>;
+  }
 
   return (
-    <div>
+    <>
       <Head>
         <title>Produtos</title>
       </Head>
 
-      <h1>Produto: {query.id}</h1>
-    </div>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} alt="" width={520} height={400} />
+        </ImageContainer>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+          <p>{product.description}</p>
+          <button>Comprar Agora</button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   );
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [{ params: { id: "prod_MUJ11RnIDwZeIM" } }],
+    fallback: true,
+  };
 };
 
-export default Product;
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  if (!params) return { props: {} };
+
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as PriceProps;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        description: product.description,
+        price: formattedCurrency({ priceInCents: price.unit_amount || 0 }),
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1hour
+  };
+};
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,73 @@
+import { styled } from "..";
+
+export const ProductContainer = styled("main", {
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  alignItems: "stretch",
+  gap: "4rem",
+  maxWidth: 1180,
+  margin: "0 auto",
+});
+
+export const ImageContainer = styled("div", {
+  width: "100%",
+  maxWidth: 576,
+  background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
+  borderRadius: 8,
+  minHeight: 656,
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  img: {
+    objectFit: "cover",
+  },
+});
+
+export const ProductDetails = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+
+  h1: {
+    fontSize: "$2xl",
+    color: "$gray300",
+  },
+
+  span: {
+    marginTop: "1rem",
+    display: "block",
+    fontSize: "$2xl",
+    color: "$green300",
+  },
+
+  p: {
+    marginTop: "2.5rem",
+    fontSize: "$md",
+    lineHeight: 1.6,
+    color: "$gray300",
+  },
+
+  button: {
+    marginTop: "auto",
+    background: "$green500",
+    border: 0,
+    color: "$white",
+    borderRadius: 8,
+    padding: "1.25rem",
+    cursor: "pointer",
+    fontSize: "$md",
+    fontWeight: "bold",
+    transition: "background 0.2s",
+
+    "&:hover": {
+      background: "$green300",
+    },
+  },
+});
+
+export const Loading = styled("h2", {
+  textAlign: "center",
+  width: "100%",
+  marginTop: "5rem",
+});
